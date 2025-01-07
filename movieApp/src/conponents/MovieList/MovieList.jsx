2944f617fb258962028f4d2a97b5581c42cbc60a
ch9729 <karlll@naver.com>
2025-01-07 15:20:52 +0900

    영화 api사용

--- movieApp/src/conponents/MovieList/MovieList.jsx
+++ movieApp/src/conponents/MovieList/MovieList.jsx
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import MovieCard from "./Moviecard";
+import _ from "lodash";
 
-const MovieList = () => {
+const MovieList = ({ type, title, emoji }) => {
+  const [movies, setMovies] = useState([]);
+  const [filterMovies, setFilterMovies] = useState([]);
+  const [minRating, setMinRating] = useState(0);
+  const [sort, setSort] = useState({
+    by: "default",
+    order: "asc",
+  });
+
   const api_key = import.meta.env.VITE_TMDB_API_KEY;
   const fetchMovies = () => {
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=ko`
+      `https://api.themoviedb.org/3/movie/${type}?api_key=${api_key}&language=ko`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -17,25 +26,39 @@ const MovieList = () => {
       .catch((err) => console.log(err));
   };
 
+  //sort 값이 업데이트 될때마다 실행
+  useEffect(() => {
+    if (sort.by !== "default") {
+      const sortedMovies = _.orderBy(filterMovies, [sort.by], [sort.order]);
+      setFilterMovies(sortedMovies);
+    }
+  }, [sort]);
+
   useEffect(() => {
     fetchMovies();
   }, []);
 
-  const [movies, setMovies] = useState([]);
-  const [filterMovies, setFilterMovies] = useState([]);
-  const [minRating, setMinRating] = useState(0);
-
   const handleFilter = (rate) => {
-    setMinRating(rate); //입력된 평점을 일단 스테이트 저장
-
-    const filtered = movies.filter((movie) => movie.vote_average >= rate);
-    setFilterMovies(filtered); // 평점 이상 영화만 저장
+    if (minRating === rate) {
+      setMinRating(0);
+      setFilterMovies(movies);
+    } else {
+      setMinRating(rate); //입력된 평점을 일단 스테이트 저장
+      const filtered = movies.filter((movie) => movie.vote_average >= rate);
+      setFilterMovies(filtered); // 평점 이상 영화만 저장
+    }
   };
+
+  const handelSort = (e) => {
+    const { name, value } = e.target;
+    setSort((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <section className="movie_list">
+    <section className="movie_list" id={`${type}`}>
       <header className="align_center movie_list_header">
         <h2 className="align_center movie_list_heading">
-          인기순 <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+          {title} <img src={emoji} alt="fire emoji" className="navbar_emoji" />
         </h2>
 
         <div className="align_center movie_list_fs">
@@ -44,7 +67,11 @@ const MovieList = () => {
               onClick={() => {
                 handleFilter(8);
               }}
-              className="movie_filter_item active"
+              className={
+                minRating === 8
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
             >
               8+ Star
             </li>
@@ -52,7 +79,11 @@ const MovieList = () => {
               onClick={() => {
                 handleFilter(7);
               }}
-              className="movie_filter_item"
+              className={
+                minRating === 7
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
             >
               7+ Star
             </li>
@@ -60,20 +91,34 @@ const MovieList = () => {
               onClick={() => {
                 handleFilter(6);
               }}
-              className="movie_filter_item"
+              className={
+                minRating === 6
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
             >
               6+ Star
             </li>
           </ul>
 
-          <select name="" id="" className="movie_sorting">
-            <option value="">SortBy</option>
-            <option value="">Date</option>
-            <option value="">Rating</option>
+          <select
+            name="by"
+            id="by"
+            onChange={handelSort}
+            className="movie_sorting"
+          >
+            <option value="default">SortBy</option>
+            <option value="release_date">Date</option>
+            <option value="vote_average">Rating</option>
           </select>
-          <select name="" id="" className="movie_sorting">
-            <option value="">Ascending</option>
-            <option value="">Descending</option>
+          <select
+            name="order"
+            id="order"
+            onChange={handelSort}
+            className="movie_sorting"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
           </select>
         </div>
       </header>
--- movieApp/src/conponents/Navbar/Navbar.jsx
+++ movieApp/src/conponents/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Fire from "../../assets/fire.png";
 import Star from "../../assets/star.png";
 import Party from "../../assets/partying-face.png";
 import "./Navbar.css";
+import DarkMode from "../DarkMode/DarkMode";
 
 const Navbar = () => {
   return (
@@ -10,16 +11,17 @@ const Navbar = () => {
       <h1>MovieApp</h1>
 
       <div className="navbar_links">
-        <a href="">
-          인기순
+        <DarkMode />
+        <a href="#popular">
+          인기작품
           <img className="navbar_emoji" src={Fire} alt="fire emoji" />
         </a>
-        <a href="">
-          평점순
+        <a href="#top_rated">
+          최고평점
           <img className="navbar_emoji" src={Star} alt="star emoji" />
         </a>
-        <a href="">
-          최신순
+        <a href="#upcoming">
+          예정작품
           <img className="navbar_emoji" src={Party} alt="party emoji" />
         </a>
       </div>
