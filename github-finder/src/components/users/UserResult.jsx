d562048d6dfe3de1a471c181c1afe64c02b84367
ch9729 <karlll@naver.com>
2025-01-08 16:02:35 +0900

    검색으로 회원 찾기

--- github-finder/src/components/users/UserResult.jsx
+++ github-finder/src/components/users/UserResult.jsx
@@ -4,10 +4,7 @@ import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 
 const UserResult = () => {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users, loading } = useContext(GithubContext);
 
   if (loading) {
     return <Spinner />;
--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -8,22 +8,25 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 // 프로바이더가 전역으로 컨텍스트를 적용함
 export const GithubProvider = ({ children }) => {
   const [users, SetUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const fetchUsers = () => {
-    const response = fetch(`${GITHUB_URL}/users`, {
+  const [loading, setLoading] = useState(false);
+  // 키워드로 유저 리스트
+  const searchUsers = (text) => {
+    setLoading(true);
+    const params = new URLSearchParams({ q: text });
+    const response = fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        SetUsers(data);
+        SetUsers(data.items);
         setLoading(false);
       })
       .catch((err) => console.log(err));
   };
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider value={{ users, loading, searchUsers }}>
       {children}
     </GithubContext.Provider>
   );
