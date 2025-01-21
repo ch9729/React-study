a581a59cd8f2cd63c707d19c06f1826c43646449
ch9729 <karlll@naver.com>
2025-01-21 16:22:23 +0900

    카테고리 스타일 수정

--- mycart/src/components/Navbar/LinkWithIcon.jsx
+++ mycart/src/components/Navbar/LinkWithIcon.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "./LinkWithIcon.css";
 
-const LinkWithIcon = ({ title, link, emoji }) => {
+const LinkWithIcon = ({ title, link, emoji, sidebar }) => {
   return (
     // 네브링크는 자동으로  active 클래스 추가됨(네브바에서 사용), href를 to로 바꾼다.
-    <NavLink to={link} className="align_center">
+    <NavLink
+      to={link}
+      className={sidebar ? `align_center category_name` : `align_center`}
+    >
       {title} <img src={emoji} alt="" className="link_emoji" />
     </NavLink>
   );
