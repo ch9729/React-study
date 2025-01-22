2d06dc4fc72341384855918375224e8f79e0c872
ch9729 <karlll@naver.com>
2025-01-22 09:37:13 +0900

    useData Hook 사용

--- mycart/src/components/products/ProductsSidebar.jsx
+++ mycart/src/components/products/ProductsSidebar.jsx
@@ -1,36 +1,27 @@
 import "./ProductsSidebar.css";
-import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
-import { useEffect, useState } from "react";
-import apiClient from "../../utils/api-client";
-import { Link } from "react-router-dom";
+import useData from "../../Hook/useData";
 
 const ProductsSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState("");
+  const { data: categories, error } = useData("category");
 
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
   return (
     <aside className="products_sidebar">
       <h2>카테고리</h2>
 
       <div className="category_links">
         {error && <em className="form_error">{error}</em>}
-        {categories.map((category) => (
-          <LinkWithIcon
-            key={category._id}
-            title={category.name}
-            link={`/products?category=${category.name}`}
-            emoji={`http://localhost:5000/category/${category.image}`}
-            sidebar={true}
-            className="custom-link-style"
-          />
-        ))}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              title={category.name}
+              link={`/products?category=${category.name}`}
+              emoji={`http://localhost:5000/category/${category.image}`}
+              sidebar={true}
+              className="custom-link-style"
+            />
+          ))}
       </div>
     </aside>
   );
